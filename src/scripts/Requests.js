c61399e93dfb163670173bc57c4e2828dc1716bf
Ben Gregory <JBGregoryPro@protonmail.com>
2021-08-13 09:50:16 -0500

    initial css buildout

---
+++ src/scripts/Requests.js
@@ -0,0 +1,27 @@
+import { getRequests } from "./dataAccess.js"
+
+
+const listItemsArray = () => {
+
+}
+
+
+
+export const Requests = () => {
+    const requests = getRequests()
+    const listItemsArray = (request) => {
+       
+         return  `<li>${request.description}</li>`
+        }
+
+        let html = ` 
+        
+        <ul>
+      
+    ${
+        requests.map(listItemsArray).join("")
+    }
+        </ul > `
+   
+    return html
+}
